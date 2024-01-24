3ec86c2b5a59ca6cf8f13e24af22c664b7de213c
Sudar37 <sudarmm2@gmail.com>
2024-01-24 15:31:28 +0530

    Task13 commit

---
+++ src/main/java/task13/ReadExcel.java
@@ -0,0 +1,44 @@
+package task13;
+
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcel {
+
+	public static void main(String[] args) throws IOException {
+
+		XSSFWorkbook book = new XSSFWorkbook("C:\\Users\\Admin\\eclipse-workspace\\ExcelFileSample\\SampleFile2.xlsx");
+		XSSFSheet sheet = book.getSheetAt(0);
+		
+		int rowCount = sheet.getLastRowNum();
+		int columnCount = sheet.getRow(0).getLastCellNum();
+		
+		String[][] data1= new String[rowCount][columnCount];
+		
+		// Get into row
+		
+		for(int i=1;i<=rowCount;i++) {
+			
+			XSSFRow row = sheet.getRow(i);
+			
+		// Get into cell
+			
+			for(int j=0;j<columnCount;j++) {
+				
+				XSSFCell cell = row.getCell(j);
+				
+				//read the data from excel
+				
+				data1[i-1][j] = cell.getStringCellValue(); 
+				
+				System.out.println(cell.getStringCellValue());	
+			}	
+		}
+		
+		book.close();	
+	}
+}
---
+++ src/main/java/task13/WriteExcel.java
@@ -0,0 +1,55 @@
+package task13;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class WriteExcel {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException{
+		// TODO Auto-generated method stub
+
+		XSSFWorkbook book = new XSSFWorkbook();
+			XSSFSheet sheet = book.createSheet();
+			
+			Object[][] data = {
+					
+					{"Name","Age","Email"},  // 1 array
+					{"John Doe",30,"john@test.com"},   
+					{"Jane Doe",28,"john@test.com"},
+					{"Bob Smith",35,"jacky@example.com"}	,
+					{"Swapnil",37,"swapnil@example.com"}	
+			};
+			
+			int rowCount =0;
+			
+			for(Object[] row1 : data) {
+				
+				XSSFRow row = sheet.createRow(rowCount++);	
+				
+				int columnCount=0;
+				
+				for(Object col : row1) {
+					
+					XSSFCell cell = row.createCell(columnCount++);
+					
+					if(col instanceof String) {
+						cell.setCellValue((String) col);
+					} else if(col instanceof Integer) {
+						cell.setCellValue((Integer) col);
+					}	
+				}
+			}
+			
+			try (
+				FileOutputStream output= new FileOutputStream("SampleFile1.xlsx"); ) {
+				book.write(output);
+			}
+		} 
+    }
+
