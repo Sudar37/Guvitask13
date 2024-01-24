d04ffde2eefd8601dfbe4a81118d8117b235efc4
Sudar37 <151596990+Sudar37@users.noreply.github.com>
2024-01-24 15:37:52 +0530

    Update WriteExcel.java

--- src/main/java/task13/WriteExcel.java
+++ src/main/java/task13/WriteExcel.java
@@ -11,45 +11,47 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class WriteExcel {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException{
-		// TODO Auto-generated method stub
+public static void main(String[] args) throws FileNotFoundException, IOException{
+	// TODO Auto-generated method stub
 
-		XSSFWorkbook book = new XSSFWorkbook();
-			XSSFSheet sheet = book.createSheet();
+	XSSFWorkbook book = new XSSFWorkbook();
+	XSSFSheet sheet = book.createSheet();
 			
-			Object[][] data = {
-					
-					{"Name","Age","Email"},  // 1 array
-					{"John Doe",30,"john@test.com"},   
-					{"Jane Doe",28,"john@test.com"},
-					{"Bob Smith",35,"jacky@example.com"}	,
-					{"Swapnil",37,"swapnil@example.com"}	
-			};
+	Object[][] data = { 
+				{"Name","Age","Email"},  
+				{"John Doe",30,"john@test.com"},   
+				{"Jane Doe",28,"john@test.com"},
+				{"Bob Smith",35,"jacky@example.com"}	,
+				{"Swapnil",37,"swapnil@example.com"}	
+			  };
 			
-			int rowCount =0;
+	int rowCount =0;
 			
-			for(Object[] row1 : data) {
+	for(Object[] row1 : data) {
 				
-				XSSFRow row = sheet.createRow(rowCount++);	
+	XSSFRow row = sheet.createRow(rowCount++);	
 				
-				int columnCount=0;
+	int columnCount=0;
 				
-				for(Object col : row1) {
+	for(Object col : row1) {
 					
-					XSSFCell cell = row.createCell(columnCount++);
+	XSSFCell cell = row.createCell(columnCount++);
 					
-					if(col instanceof String) {
-						cell.setCellValue((String) col);
-					} else if(col instanceof Integer) {
-						cell.setCellValue((Integer) col);
-					}	
-				}
-			}
+	if(col instanceof String) {
+	cell.setCellValue((String) col);
+	} else if(col instanceof Integer) {
+	cell.setCellValue((Integer) col);
+     }	
+  }
+}
 			
-			try (
-				FileOutputStream output= new FileOutputStream("SampleFile1.xlsx"); ) {
-				book.write(output);
-			}
-		} 
-    }
+	try (
+	FileOutputStream output= new FileOutputStream("SampleFile1.xlsx"); ) {
+	book.write(output);
+	}
+    } 
+ }
+---------------------------------------------------------------------------------------------------------------------
+OUTPUT:
 
+Write data to an Excel file "SampleFile1.xlsx" successfully.
