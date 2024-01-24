b42fe8798f2e85fecf34a283e61dab3b037d7faa
Sudar37 <151596990+Sudar37@users.noreply.github.com>
2024-01-24 15:39:32 +0530

    Update ReadExcel.java

--- src/main/java/task13/ReadExcel.java
+++ src/main/java/task13/ReadExcel.java
@@ -9,7 +9,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ReadExcel {
 
-	public static void main(String[] args) throws IOException {
+public static void main(String[] args) throws IOException {
 
 		XSSFWorkbook book = new XSSFWorkbook("C:\\Users\\Admin\\eclipse-workspace\\ExcelFileSample\\SampleFile2.xlsx");
 		XSSFSheet sheet = book.getSheetAt(0);
@@ -23,22 +23,46 @@ public class ReadExcel {
 		
 		for(int i=1;i<=rowCount;i++) {
 			
-			XSSFRow row = sheet.getRow(i);
+		XSSFRow row = sheet.getRow(i);
 			
 		// Get into cell
 			
-			for(int j=0;j<columnCount;j++) {
+		for(int j=0;j<columnCount;j++) {
 				
-				XSSFCell cell = row.getCell(j);
+		XSSFCell cell = row.getCell(j);
 				
-				//read the data from excel
+		//read the data from excel
 				
-				data1[i-1][j] = cell.getStringCellValue(); 
+		data1[i-1][j] = cell.getStringCellValue(); 
 				
-				System.out.println(cell.getStringCellValue());	
-			}	
-		}
+		System.out.println(cell.getStringCellValue());	
+    }	
+}
 		
 		book.close();	
-	}
+   }
 }
+-----------------------------------------------------------------------------------------------------------------
+OUTPUT:
+
+ERROR StatusLogger Log4j2 could not find a logging implementation. Please add log4j-core to the classpath. Using SimpleLogger to log to the console...
+Mohan
+231
+25
+Banglore
+Ram
+232
+32
+Coimbatore
+Mano
+233
+28
+Trichy
+Sree
+234
+30
+Chennai
+Karthick
+235
+30
+Chennai
